a94e6da8e82c73b7011a9f12052597b6b4c5e8f0
Mahmoud Hussein <111658772+kottp6@users.noreply.github.com>
2025-06-19 18:08:53 +0300

    seven commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { AnimatePresence } from "framer-motion";
-import './App.css'
+import './App.css';
 import About from "./components/About";
 import Services from './components/Services';
 import Products from './components/Products';
 import Contact from './components/Contact';
 import SplashScreen from './pages/splashScreen';
 import Home from './components/Home';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Partners from './components/Partners';
+import NotFound from './components/NotFound';
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // changed here
 import { Toaster } from 'react-hot-toast';
-import NotFound from './components/NotFound'
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -18,7 +18,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2500); // 2.5 seconds
+    }, 2500);
     return () => clearTimeout(timer);
   }, []);
 
@@ -34,16 +34,15 @@ function App() {
     <>
       <Toaster position="top-center" />
       <Routes>
-        <Route index={true}  element={<Home />} />
+        <Route index={true} element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/products" element={<Products />} />
-        <Route path='/partners' element={<Partners />} />
+        <Route path="/partners" element={<Partners />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound/>} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      </>
-    
+    </>
   );
 }
 
